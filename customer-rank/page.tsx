bad2a7647149df6be206561448a0603bc04c5d83
patrikkarsa444 <dedokarsa@gmail.com>
2025-02-20 16:17:31 +0100

    Update page.tsx

--- customer-rank/page.tsx
+++ customer-rank/page.tsx
@@ -1,18 +1,15 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-import Layout from "../layout"
 
 export default function CustomerRankPage() {
   return (
-    <Layout>
-      <Card>
-        <CardHeader>
-          <CardTitle>Customer Rank</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <p>Customer ranking system will be implemented here.</p>
-        </CardContent>
-      </Card>
-    </Layout>
+    <Card>
+      <CardHeader>
+        <CardTitle>Customer Rank</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p>Customer ranking system will be implemented here.</p>
+      </CardContent>
+    </Card>
   )
 }
 
