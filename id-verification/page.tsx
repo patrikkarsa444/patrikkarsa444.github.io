482fec8ab5150f90f3ac823cefe60e12e903f553
patrikkarsa444 <dedokarsa@gmail.com>
2025-02-20 16:17:02 +0100

    Update page.tsx

--- id-verification/page.tsx
+++ id-verification/page.tsx
@@ -1,18 +1,15 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-import Layout from "../layout"
 
 export default function IDVerificationPage() {
   return (
-    <Layout>
-      <Card>
-        <CardHeader>
-          <CardTitle>ID Verification</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <p>ID verification functionality will be implemented here.</p>
-        </CardContent>
-      </Card>
-    </Layout>
+    <Card>
+      <CardHeader>
+        <CardTitle>ID Verification</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p>ID verification functionality will be implemented here.</p>
+      </CardContent>
+    </Card>
   )
 }
 
