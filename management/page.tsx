cf34146d235dc0099bc628846d8847c1b761cddc
patrikkarsa444 <dedokarsa@gmail.com>
2025-02-20 16:17:58 +0100

    Update page.tsx

--- management/page.tsx
+++ management/page.tsx
@@ -1,18 +1,15 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-import Layout from "../layout"
 
 export default function ManagementPage() {
   return (
-    <Layout>
-      <Card>
-        <CardHeader>
-          <CardTitle>Management</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <p>System management tools will be implemented here.</p>
-        </CardContent>
-      </Card>
-    </Layout>
+    <Card>
+      <CardHeader>
+        <CardTitle>Management</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p>System management tools will be implemented here.</p>
+      </CardContent>
+    </Card>
   )
 }
 
